aa101a2b15d725adb1bfb5fe2110c46b5631557c
Inkeister <truongduchainguyen@gmail.com>
2022-06-16 10:51:51 +0700

    config env dev + prod

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
+// Middleware
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
