ac76613325912adcf714d7a3acc2c62b183ad534
Inkeister <truongduchainguyen@gmail.com>
2022-06-17 21:47:01 +0700

    refactoring to MVC and add mongodb config

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
